d6c65abd1dea8bfe7b52da00dae7826523b42d18
Richard Ekholm <richard.ekholm@elev.ga.ntig.se>
2024-10-21 09:56:06 +0200

    ioh4egfiuh

---
+++ recruitment/Program.cs
@@ -0,0 +1,41 @@
+﻿List<string> personer=new List<string>(){"Sigma", "Jonkler", "Man", "Man-bat"};
+List<string> rekryter = new List<string>();
+
+
+
+
+while (personer.Count > 0)
+{
+
+Console.WriteLine("Dessa är de du kan välja mellan :) ");
+for (int jhon = 0; jhon <personer.Count ; jhon ++){
+    Console.Write($"{jhon+1}. ");
+    Console.WriteLine(personer[jhon]);
+}
+
+}
+
+Console.WriteLine("Välj vem du vill anställa för att slåss för mahtog genom att ange deras nummer, klicka 0 för att avsluta rekryteringen");
+string input = Console.ReadLine();
+
+int i;
+
+bool sucess = int.TryParse(input, out i);
+
+if (i == 0){
+    Console.WriteLine("Rekrytering avslutad, mahtog är cooked gg wp gp next");
+    System.Environment.Exit(0);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+Console.ReadLine();
